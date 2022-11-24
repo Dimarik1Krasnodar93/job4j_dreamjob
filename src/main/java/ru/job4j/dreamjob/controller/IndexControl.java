3235059f1ac6a0584e482cb11c3cc4412a2ae17b
Dimarik <krasnodar93@mail.ru>
2022-11-24 17:12:07 +0300

    Hellow Word!

--- src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexControl.java
@@ -2,12 +2,27 @@ package ru.job4j.dreamjob.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.yaml.snakeyaml.reader.StreamReader;
+
+
+import java.io.*;
+import java.util.Properties;
 
 @RestController
 public class IndexControl {
 
     @GetMapping("/index")
     public String index() {
-        return "Greetings from Spring Boot!";
+        StringBuilder indexBuilder = new StringBuilder();
+        final ClassLoader loader = IndexControl.class.getClassLoader();
+        int read;
+        try (InputStream io = loader.getResourceAsStream("templates/index.html")) {
+            while ((read = io.read()) != -1) {
+                indexBuilder.append((char) read);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return indexBuilder.toString();
     }
 }
