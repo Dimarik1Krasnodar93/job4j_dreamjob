442121003b924883b14f61d2347582ed1cafe7f1
Dimarik <krasnodar93@mail.ru>
2022-12-14 08:10:22 +0300

    удаление файлов2

--- src/main/java/ru/job4j/dreamjob/queries/CandidateQueries.java
+++ src/main/java/ru/job4j/dreamjob/queries/CandidateQueries.java
@@ -3,7 +3,7 @@ package ru.job4j.dreamjob.queries;
 public class CandidateQueries {
     public static final String UPDATE = "UPDATE CANDIDATE set name = ?, description = ?, created = ?, "
             + "visible = ?, id_city = ?, photo = ? WHERE id = ?";
-    public static final String ADD = "INSERT INTO CANDIDATE (name, description, created, visible, id_city, photo) VALUES (?, ?, ?, ?, ?, ?)";
+    public static final String ADD = "INSERT INTO CANDIDATE (name, description, created, visible, city_id, photo) VALUES (?, ?, ?, ?, ?, ?)";
     public static final String FIND = "SELECT * FROM CANDIDATE WHERE id = ?";
     public static final String FIND_ALL = "SELECT * FROM CANDIDATE";
 }
